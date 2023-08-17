7bbbf1b48b845b7e428a359bdc2c162000f069f1
Joachim Grill <joachim@codeandweb.com>
2023-08-17 18:22:55 +0200

    renamed script; added webgl build target

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class walk : MonoBehaviour
+public class PlayerMove : MonoBehaviour
 {
 	Animator anim;
 	SpriteRenderer sprite;
